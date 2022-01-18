283b903b6d7828729412075a280c9620c20be658
vinylrich <whktjd0109@gmail.com>
2022-01-18 09:40:27 +0900

    todos

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,28 +1,30 @@
 import Button from "./Button";
-import { useState , useEffect} from "react";
+import { useState , useEffect } from "react";
+
 
 function App() {
-    const [counter,setValue] = useState(0)
-    const [keyword,setKeyword] = useState("")
-    const onClick = () => setValue((prev) => prev+1);
-    const onChange = (event)=> setKeyword(event.target.value)
-    useEffect(() => {
-        console.log("i run when keyword changes")
-    },[keyword])
-    useEffect( ()=>{
-        console.log("i run when counter changes")
-    },[counter])
-  return (
-    <div className="App">
-        <input
-            value= {keyword}
-            onChange={onChange}
-            type = "text"
-            placeholder="Search here.."/>
-        <h1>{counter}</h1>
-        <button onClick={onClick}>click me</button>
-    </div>
-  );
+    const [todo,setTodo] = useState("");
+    const [todos,setTodos] = useState([])
+    const onChange = (event) => setTodo(event.target.value); // setTodo -> todo를 인자값으로 바꾸는 것(여기서는 EVENT.TARGET.VALUE)
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (todo == "") {
+            return;
+        }
+        setTodos(currentArray => [todo,...currentArray])
+        setTodo("")
+    };
+    return (
+        <div>
+            <h1>Todos ({todos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input value = {todo} onChange={onChange} type="text" placeholder="write your to do.."/>
+                <button>Add to Do</button>
+            </form>
+            <hr/>
+            {todos.map((todo,index) => <li key={index}>{todo}</li>)}
+        </div>
+    );
 }
 //use effect를 사용하면 지정한 변수가 바뀔때만 실행하는 함수를 사용할 수 있다.
 export default App;
